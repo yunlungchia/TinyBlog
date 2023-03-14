cff5a54d11ead87fadbb1b43723f5775d0dda374
yunlungchia <chiayunlung@gmail.com>
2023-03-14 23:06:12 +0800

    docs(README.md)

--- src/main/java/com/personal/blog/controller/IndexController.java
+++ src/main/java/com/personal/blog/controller/IndexController.java
@@ -12,6 +12,4 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     private static final Logger log = LoggerFactory.getLogger(IndexController.class);
-
-    public Result<>
 }
