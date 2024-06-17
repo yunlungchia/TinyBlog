72c9e940be5d68f3c4ca751c41cf0aff7eea3551
yunlungchia <chiayunlung@gmail.com>
2024-06-18 00:21:52 +0800

    func(*): update

--- src/main/resources/control.sh
+++ src/main/resources/control.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 启动
-exec java -javaagent:/Users/yunlungchia/sandbox/lib/sandbox-agent.jar -jar /Users/yunlungchia/Documents/TinyBlog/target/blog-0.0.1-SNAPSHOT.jar > /Users/yunlungchia/Documents/JavaApplication.log
+exec java -javaagent:/Users/yunlungchia/sandbox/lib/sandbox-agent.jar=server.port=12580 -jar /Users/yunlungchia/Documents/TinyBlog/target/blog-0.0.1-SNAPSHOT.jar > /Users/yunlungchia/JavaApplication.log
 
 # 终止
 ps -ef | grep "blog-0.0.1-SNAPSHOT.jar"
